ecccc81b4ae020d5166efa7ecdb3c933d3ee8569
vdeka <vdeka@etech360.com>
2019-11-02 21:11:51 -0500

    redirect unauthorized

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -2,7 +2,7 @@ import auth from '../service/auth';
 
 export const checkErrorMessage = (message) => {
     console.log(message);
-   if(message.includes("Unauthorized") || message.includes("JWT")){
+   if(message.includes("Unauthorized") || message.includes("JWT") || message.includes('Unable to read JSON value: {"alg')){
        auth.cleanToken();
    }
 };
